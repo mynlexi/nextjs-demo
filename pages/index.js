1c8f4fd82c04bbf444bf98e740855033023d9547
lorenz westner <mynlexi@gmail.com>
2021-01-07 11:55:05 +0100

    second push

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ export default function Home({allPostsData}) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Web Developer</p>
+        <p>Web Developer // My overton window is bigger than yours</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
